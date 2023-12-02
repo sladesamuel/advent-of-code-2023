0ced272790f86bae2592e4fcdb551e3c6da1c23d
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2023-12-02 21:52:03 +0000

    Implemented part 2 logic

--- day-02/src/index.ts
+++ day-02/src/index.ts
@@ -1,12 +1,18 @@
 import { readFileSync } from "fs"
-import run from "./game/runPart1"
+import runPart1 from "./game/runPart1"
+import runPart2 from "./game/runPart2"
 
 const [, , part, inputFilePath] = process.argv
 
 const input = readFileSync(inputFilePath).toString()
 
 if (part === "part1") {
-  const result = run(input)
+  const result = runPart1(input)
 
   console.log(`The result for Part 1 is: ${result}`)
+}
+else {
+  const result = runPart2(input)
+
+  console.log(`The result for Part 2 is: ${result}`)
 }
