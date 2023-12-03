0e92a61a6c2a98b4db361996a1a31a1fab816cb1
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2023-12-03 21:43:45 +0000

    Run part 1

--- day-03/src/filterParts.test.ts
+++ day-03/src/filterParts.test.ts
@@ -11,37 +11,35 @@ const createPart = (partNumber: number, row: number, ...columns: number[]): Part
 describe("filterParts()", () => {
   it("should return the filtered set of parts", () => {
     /*
-     * ...45
-     * ..4*.
-     * .....
-     * 3...4
-     * +....
+     * 467..114..
+     * ...*......
+     * ..35..633.
+     * ......#...
      */
 
     const grid: Grid = {
-      rows: 5,
-      columns: 5,
+      rows: 4,
+      columns: 10,
       cells: [
-        [".", ".", ".", 4, 5],
-        [".", ".", 5, "*", "."],
-        [".", ".", ".", ".", "."],
-        [3, ".", ".", ".", 4],
-        ["+", ".", ".", ".", "."]
+        [4, 6, 7, ".", ".", 1, 1, 4, ".", "."],
+        [".", ".", ".", "*", ".", ".", ".", ".", ".", "."],
+        [".", ".", 3, 5, ".", ".", 6, 3, 3, "."],
+        [".", ".", ".", ".", ".", ".", "#", ".", ".", "."]
       ]
     }
 
     const parts: Part[] = [
-      createPart(45, 0, 3, 4),
-      createPart(5, 1, 2),
-      createPart(3, 3, 0),
-      createPart(4, 3, 4)
+      createPart(467, 0, 0, 1, 2),
+      createPart(114, 0, 5, 6, 7),
+      createPart(35, 2, 2, 3),
+      createPart(633, 2, 5, 6, 7)
     ]
 
     const filtered = filterParts(grid, parts)
 
     expect(filtered).toHaveLength(3)
-    expect(filtered[0].partNumber).toEqual(45)
-    expect(filtered[1].partNumber).toEqual(5)
-    expect(filtered[2].partNumber).toEqual(3)
+    expect(filtered[0].partNumber).toEqual(467)
+    expect(filtered[1].partNumber).toEqual(35)
+    expect(filtered[2].partNumber).toEqual(633)
   })
 })
