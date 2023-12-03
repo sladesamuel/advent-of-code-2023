7e7ba791918fee58e31b47bb1be891e4f5cc23ff
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2023-12-03 21:48:29 +0000

    Dummy input

--- day-03/src/index.ts
+++ day-03/src/index.ts
@@ -1,1 +1,9 @@
-console.log("TODO")
+import { readFileSync } from "fs"
+import runPart1 from "./runPart1"
+
+const [, , inputFilePath] = process.argv
+
+const input = readFileSync(inputFilePath).toString()
+const result = runPart1(input)
+
+console.log(`Part 1 result = ${result}`)
